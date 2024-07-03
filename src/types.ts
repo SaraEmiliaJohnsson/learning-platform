a7641a9bd0d8da12a79a7305e12d5d63c692b6fc
Sara Johnsson <sara86persson@hotmail.com>
2024-07-03 21:13:24 +0200

    added types.ts and function to get ongoing and completed courses

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+
+export interface Course {
+    id: string;
+    title: string;
+    description: string;
+    completed: boolean;
+    students: string[];
+    assignments: string[];
+}
--- src/views/landingpage/StudentLandingPage.tsx
+++ src/views/landingpage/StudentLandingPage.tsx
@@ -1,12 +1,42 @@
 import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-import { auth } from "../../components/firebase/FirebaseConfig";
+import { auth, db } from "../../components/firebase/FirebaseConfig";
 import './StudentLandingPage.css';
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../../components/auth/AuthContext";
+import { collection, doc, getDocs, query, where } from "firebase/firestore";
+import { Course } from "../../types";
 
 
 
-const StudentLandingPage = () => {
+const StudentLandingPage: React.FC = () => {
+    const { currentUser } = useAuth();
+    const [courses, setCourses] = useState<Course[]>([]);
+    const [completedCourses, setCompletedCourses] = useState<Course[]>([]);
+    const [messages, setMessages] = useState([]);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchCourses = async () => {
+            if (!currentUser) return;
+
+            const q = query(collection(db, 'courses'), where('students', 'array-contains', currentUser?.uid));
+            const querySnapshot = await getDocs(q);
+            const ongoing: Course[] = [];
+            const completed: Course[] = [];
+            querySnapshot.forEach((doc) => {
+                const data = doc.data() as Omit<Course, 'id'>;
+                const course = { id: doc.id, ...data };
+                if (course.completed) {
+                    completed.push(course);
+                } else {
+                    ongoing.push(course);
+                }
+            });
+            setCourses(ongoing);
+            setCompletedCourses(completed);
+        };
+    })
 
     const handleSignOut = async () => {
 
@@ -28,7 +58,22 @@ const StudentLandingPage = () => {
                 <button className="logout-button" onClick={handleSignOut}>Logga ut</button>
             </header>
             <main className="landingpage-main">
-
+                <div>
+                    <h3>Pågående Kurser</h3>
+                    <ul>
+                        {courses.map((course) => (
+                            <li key={course.id}>{course.title}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div>
+                    <h3>Avslutade Kurser</h3>
+                    <ul>
+                        {completedCourses.map((course) => (
+                            <li key={course.id}>{course.title}</li>
+                        ))}
+                    </ul>
+                </div>
             </main>
 
         </section>
